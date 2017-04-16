f307bac2c04c99428babc0bca11c732fce885049
konstantin8105 <konstantin8105@gmail.com>
2017-04-16 11:20:32 +0300

    separate the inp ganaration

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/Konstantin8105/ShellResearch/research"
+import (
+	"fmt"
+
+	"github.com/Konstantin8105/CalculixRPCclient/clientCalculix"
+	"github.com/Konstantin8105/ShellResearch/research"
+)
 
 func main() {
+	inp, err := research.ShellModel(3., 5., 10, 10, 1., 0.005)
+	if err != nil {
+		fmt.Println("Cannot mesh")
+		return
+	}
+	var client clientCalculix.ClientCalculix
+	client.Manager = *clientCalculix.NewServerManager()
+	factors, err := client.CalculateForBuckle([]string{inp})
+	for i, factor := range factors {
+		fmt.Println("Factor ", i, " -- ", factor)
+	}
 	research.RC002()
 }
--- research/researchRC002.go
+++ research/researchRC002.go
@@ -3,9 +3,9 @@ package research
 import (
 	"fmt"
 	"math"
-	"os"
 	"path/filepath"
 
+	"github.com/Konstantin8105/CalculixRPCclient/clientCalculix"
 	"github.com/Konstantin8105/Convert-INP-to-STD-format/inp"
 	"github.com/Konstantin8105/Shell_generator/shellGenerator"
 	"github.com/gonum/plot"
@@ -22,24 +22,12 @@ func RC002() {
 
 	diameter := 3.
 	height := 1.
-	precision := 0.5
 	pointsOnLevel := 5
 	pointsOnHeight := 5
 	force := -1.0
 	thk := 0.005
-	modelFileName, err := createResearchFilename(researchName, "model.inp")
-	if err != nil {
-		fmt.Println("Wrong name of model : ", err)
-		return
-	}
-	// get result of buckling factor
-	dat, err := createResearchFilename(researchName, "model.dat")
-	if err != nil {
-		fmt.Println("Wrong file .dat: ", err)
-		return
-	}
 
-	n := 10
+	n := 40
 
 	calcTime := make(plotter.XYs, n)
 
@@ -53,113 +41,32 @@ func RC002() {
 	p.Y.Label.Text = "Calculation time,sec"
 
 	for iteration := 0; iteration < n; iteration++ {
-
-		_ = os.Remove(modelFileName)
-		_ = os.Remove(dat)
-
-		// create cylinder model
-		model, err := (shellGenerator.Shell{Height: height, Diameter: diameter, Precision: precision}).GenerateMesh(pointsOnLevel, pointsOnHeight)
+		model, err := ShellModel(height, diameter, pointsOnLevel, pointsOnHeight, force, thk)
 		if err != nil {
-			fmt.Println("Wrong mesh : ", err)
+			fmt.Println("Cannot mesh")
 			return
 		}
-
-		// create fixed points
-		fixName := "fix"
-		model.AddNamedNodesOnLevel(0, fixName)
-		model.Boundary = append(model.Boundary, inp.BoundaryProperty{
-			NodesByName:   fixName,
-			StartFreedom:  1,
-			FinishFreedom: 1,
-			Value:         0,
-		})
-		model.Boundary = append(model.Boundary, inp.BoundaryProperty{
-			NodesByName:   fixName,
-			StartFreedom:  2,
-			FinishFreedom: 2,
-			Value:         0,
-		})
-		model.Boundary = append(model.Boundary, inp.BoundaryProperty{
-			NodesByName:   fixName,
-			StartFreedom:  3,
-			FinishFreedom: 3,
-			Value:         0,
-		})
-
-		// create load points
-		loadName := "load"
-		model.AddNamedNodesOnLevel(height, loadName)
-		model.Boundary = append(model.Boundary, inp.BoundaryProperty{
-			NodesByName:   loadName,
-			StartFreedom:  1,
-			FinishFreedom: 1,
-			Value:         0,
-		})
-		model.Boundary = append(model.Boundary, inp.BoundaryProperty{
-			NodesByName:   loadName,
-			StartFreedom:  3,
-			FinishFreedom: 3,
-			Value:         0,
-		})
-		forcePerPoint := force / float64(pointsOnLevel)
-		model.Step.Loads = append(model.Step.Loads, inp.Load{
-			NodesByName: loadName,
-			Direction:   2,
-			LoadValue:   forcePerPoint,
-		})
-
-		// addshell property
-		model.ShellSections = append(model.ShellSections, inp.ShellSection{
-			ElementName: shellGenerator.ShellName,
-			Thickness:   thk,
-		})
-
-		// create linear buckling
-		model.Step.AmountBucklingShapes = 1
-
-		// save file
-		err = model.Save(modelFileName)
-		if err != nil {
-			fmt.Println("Wrong saving : ", err)
-			return
+		var client clientCalculix.ClientCalculix
+		client.Manager = *clientCalculix.NewServerManager()
+		dats, err := client.CalculateForBuckle([]string{model})
+		for _, dat := range dats {
+			fmt.Println(dat)
 		}
 
-		// calculate
-		err = executeCalculix(modelFileName)
-		if err != nil {
-			fmt.Println("Wrong execution : ", err)
-			return
-		}
+		bucklingFactors := dats[0]
 
-		bucklingFactors, err := getBucklingFactor(dat, model.Step.AmountBucklingShapes)
-		if err != nil {
-			fmt.Println("Wrong buckling factor analyze : ", err)
-			return
-		}
-
-		// calculate the buckling force
-		//fmt.Printf("%.4v\t%.4v\n", diameter, bucklingFactors[0])
 		// calculate by Timoshenko formula
 		timoshenkoStress := 0.605 * 2.e11 * thk / (diameter / 2.0)
 		timoshenkoForce := timoshenkoStress * math.Pi * diameter * thk
-		stress2 := 750. * math.Pow(math.Pi, 2.) * 2e11 / (12. * (1. - 0.3*0.3)) * math.Pow(thk/height, 2.)
-		force2 := stress2 * math.Pi * diameter * thk
+		//stress2 := 750. * math.Pow(math.Pi, 2.) * 2e11 / (12. * (1. - 0.3*0.3)) * math.Pow(thk/height, 2.)
+		//force2 := stress2 * math.Pi * diameter * thk
 		// compare the result
 
 		size := math.Pi * diameter * height / float64(pointsOnLevel) / float64(pointsOnHeight)
-		fmt.Printf("%v\t%.4v\t%.4v\t%.4v\t%.4v\n", size, thk, diameter, force*bucklingFactors[0], timoshenkoForce)
-
-		for i := 0; i < model.Step.AmountBucklingShapes; i++ {
-			fmt.Printf("%.4f\t", bucklingFactors[i])
-		}
-		fmt.Println("")
-
-		fmt.Println("onLevel = ", pointsOnLevel, "\tlevels = ", pointsOnHeight)
-
-		fmt.Println("Buckling  2 : = ", force2)
+		fmt.Printf("%v\t%.4v\t%.4v\t%.4v\t%.4v\n", size, thk, diameter, force*bucklingFactors, timoshenkoForce)
 
 		calcTime[iteration].X = float64(iteration) // float64(pointsOnHeight) //size
-		calcTime[iteration].Y = math.Abs(force * bucklingFactors[0])
+		calcTime[iteration].Y = math.Abs(force * bucklingFactors)
 
 		pointsOnLevel += 5  //int(float64(pointsOnLevel) * 1.05)
 		pointsOnHeight += 5 //int(float64(pointsOnHeight) * 1.1)
@@ -177,4 +84,78 @@ func RC002() {
 	if err := p.Save(8*vg.Inch, 8*vg.Inch, string(researchFolder+string(filepath.Separator)+researchName+string(filepath.Separator)+researchName+".png")); err != nil {
 		panic(err)
 	}
+}
+
+// ShellModel - shell model
+func ShellModel(height float64, diameter float64, pointsOnLevel, pointsOnHeight int, force, thk float64) (resultInp string, err error) {
+	var model inp.Format
+
+	precision := 0.5
+
+	// create cylinder model
+	model, err = (shellGenerator.Shell{Height: height, Diameter: diameter, Precision: precision}).GenerateMesh(pointsOnLevel, pointsOnHeight)
+	if err != nil {
+		fmt.Println("Wrong mesh : ", err)
+		return
+	}
+
+	// create fixed points
+	fixName := "fix"
+	model.AddNamedNodesOnLevel(0, fixName)
+	model.Boundary = append(model.Boundary, inp.BoundaryProperty{
+		NodesByName:   fixName,
+		StartFreedom:  1,
+		FinishFreedom: 1,
+		Value:         0,
+	})
+	model.Boundary = append(model.Boundary, inp.BoundaryProperty{
+		NodesByName:   fixName,
+		StartFreedom:  2,
+		FinishFreedom: 2,
+		Value:         0,
+	})
+	model.Boundary = append(model.Boundary, inp.BoundaryProperty{
+		NodesByName:   fixName,
+		StartFreedom:  3,
+		FinishFreedom: 3,
+		Value:         0,
+	})
+
+	// create load points
+	loadName := "load"
+	model.AddNamedNodesOnLevel(height, loadName)
+	model.Boundary = append(model.Boundary, inp.BoundaryProperty{
+		NodesByName:   loadName,
+		StartFreedom:  1,
+		FinishFreedom: 1,
+		Value:         0,
+	})
+	model.Boundary = append(model.Boundary, inp.BoundaryProperty{
+		NodesByName:   loadName,
+		StartFreedom:  3,
+		FinishFreedom: 3,
+		Value:         0,
+	})
+	forcePerPoint := force / float64(pointsOnLevel)
+	model.Step.Loads = append(model.Step.Loads, inp.Load{
+		NodesByName: loadName,
+		Direction:   2,
+		LoadValue:   forcePerPoint,
+	})
+
+	// addshell property
+	model.ShellSections = append(model.ShellSections, inp.ShellSection{
+		ElementName: shellGenerator.ShellName,
+		Thickness:   thk,
+	})
+
+	// create linear buckling
+	model.Step.AmountBucklingShapes = 1
+
+	lines := model.SaveINPtoLines()
+	var buffer string
+	for _, line := range lines {
+		buffer += line + "\n"
+	}
+	return buffer, nil
 }
