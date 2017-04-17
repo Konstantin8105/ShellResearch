a4f6cb22e0f9a426c5ca95f7ac75c250352ded5f
konstantin8105 <konstantin8105@gmail.com>
2017-04-17 18:16:33 +0300

    merge

--- research/researchRC002.go
+++ research/researchRC002.go
@@ -27,7 +27,7 @@ func RC002() {
 	force := -1.0
 	thk := 0.005
 
-	n := 10
+	n := 20
 
 	calcTime := make(plotter.XYs, n)
 
@@ -43,6 +43,8 @@ func RC002() {
 	var inpModels []string
 
 	for iteration := 0; iteration < n; iteration++ {
+		fmt.Println("Prepare model : ", iteration)
+
 		model, err := ShellModel(height, diameter, pointsOnLevel, pointsOnHeight, force, thk)
 		if err != nil {
 			fmt.Println("Cannot mesh")
