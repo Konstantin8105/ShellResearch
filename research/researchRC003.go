b7bb5f2d86e14cc0196507d5a88db1cfe7359b6b
Konstantin8105 <pollygon2011>
2017-04-20 12:51:18 +0300

    change graph colos

--- research/researchRC003.go
+++ research/researchRC003.go
@@ -80,7 +80,7 @@ func RC003() {
 			R: uint8(255. * (float64(i) / float64(n))),
 			G: uint8(255. * (1. - float64(i)/float64(n))),
 			B: 0,
-			A: 200,
+			A: 0,
 		}
 		// Add the plotters to the plot, with a legend
 		// entry for each
